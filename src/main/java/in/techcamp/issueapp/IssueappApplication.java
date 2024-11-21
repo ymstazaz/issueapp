f112fe2251203795d0c5184888ce27ce17564f2a
ymstazaz <r37a45d@gmail.com>
2024-11-21 16:25:08 +0900

    first

---
+++ src/main/java/in/techcamp/issueapp/IssueappApplication.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/issueapp/IssueappApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
